5ecbb146cdba93eac7600c05a625b761bb5b6783
riptide16 <abdulhafidh.perpusnas@gmail.com>
2024-05-07 09:37:43 +0700

    Layout Public Fix

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -17,7 +17,7 @@
 			<div x-data="{ open: false }" class="menu-mobile-hide flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
 			  <div class="p-4 flex flex-row items-center justify-between">
 				<a href="{{ route('home.index') }}" class="text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline">
-					<img src="{{ asset('images/logo.png') }}" alt="Logo" class="w-60 mr-10">
+					<img src="{{ asset('images/Logo_SiPAPI.png') }}" alt="Logo" class="w-60 mr-10">
 				</a>
 				<button class="md:hidden rounded-lg focus:outline-none focus:shadow-outline" @click="open = !open">
 				  <svg fill="currentColor" viewBox="0 0 20 20" class="w-6 h-6">
@@ -34,7 +34,7 @@
                         <span>{{ $menu['name'] }}</span>
                         <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                         </button>
-                        <div x-show="open" x-transition:enter="transition ease-out duration-300" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="z-50 absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48">
+                        <div x-show="open" x-transition:enter="transition ease-out duration-300" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="z-50 absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48" style="display: none">
                             <div class="px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-800">
                                 @foreach ($menu['children'] as $submenu)
                                     <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="{{ $submenu['is_default'] == true ? route('home.index').'/'.$submenu['url'] : route('home.dynamicMenu', ['parent' => explode("/", $submenu['url'])[0], 'child' => explode("/", $submenu['url'])[1]]) }}">{{ $submenu['name'] }}</a>
